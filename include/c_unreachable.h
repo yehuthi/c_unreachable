52eb7a951788c3a933e9435ae0813e4f41f3da62
yehuthi <yehuthith@gmail.com>
2024-08-23 10:56:55 +0300

    improve C23 branch
    Make sure the C23 implementation actually provides `unreachable`.

--- include/c_unreachable.h
+++ include/c_unreachable.h
@@ -1,9 +1,14 @@
 #ifndef __C_UNREACHABLE_H
 #define __C_UNREACHABLE_H
 
-// C23
+// C23 setup
 #if __STDC_VERSION__ >= 202311L
 #include <stddef.h>
+#endif
+
+// C23: check if actually implemented, as C23 is still pretty new
+#if __STDC_VERSION__ >= 202311L && defined(unreachable)
+// then unreachable is provided, do nothing.
 
 // Compiler extensions
 #elif defined(__GNUC__)
