2d7d3898b541ad2f8a33925364ccab77fc85bbfe
yehuthi <yehuthith@gmail.com>
2024-08-23 11:50:12 +0300

    simplify diagnostics branching

--- include/c_unreachable.h
+++ include/c_unreachable.h
@@ -10,7 +10,6 @@
 #if __STDC_VERSION__ > 201710L && defined(unreachable)
 // then unreachable is provided, do nothing.
 // Diagnostics {{{
-#ifdef C_UNREACHABLE_DIAG
 #if C_UNREACHABLE_DIAG == 1
 #error c_unreachable method: C23
 #elif C_UNREACHABLE_DIAG == 2
@@ -18,14 +17,12 @@
 #elif C_UNREACHABLE_DIAG == 3
 #pragma message("c_unreachable method: C23")
 #endif
-#endif
 // }}}
 
 // Compiler extensions
 #elif defined(__GNUC__)
 #define unreachable() (__builtin_unreachable())
 // Diagnostics {{{
-#ifdef C_UNREACHABLE_DIAG
 #if C_UNREACHABLE_DIAG == 1
 #error c_unreachable method: extension GNU
 #elif C_UNREACHABLE_DIAG == 2
@@ -33,19 +30,16 @@
 #elif C_UNREACHABLE_DIAG == 3
 #pragma message("c_unreachable method: extension GNU")
 #endif
-#endif
 // }}}
 #elif defined(_MSC_VER)
 #define unreachable() (__assume(0))
 // Diagnostics {{{
-#ifdef C_UNREACHABLE_DIAG
 #if C_UNREACHABLE_DIAG == 1
 #error c_unreachable method: extension MSVC
 #elif C_UNREACHABLE_DIAG == 2
 #warning c_unreachable method: extension MSVC
 #elif C_UNREACHABLE_DIAG == 3
 #pragma message("c_unreachable method: extension MSVC")
-#endif
 #endif
 // }}}
 
@@ -54,7 +48,6 @@
 _Noreturn inline void __c_unreachable_impl() {}
 #define unreachable() (__c_unreachable_impl())
 // Diagnostics {{{
-#ifdef C_UNREACHABLE_DIAG
 #if C_UNREACHABLE_DIAG == 1
 #error c_unreachable method: noreturn
 #elif C_UNREACHABLE_DIAG == 2
@@ -62,20 +55,17 @@ _Noreturn inline void __c_unreachable_impl() {}
 #elif C_UNREACHABLE_DIAG == 3
 #pragma message("c_unreachable method: noreturn")
 #endif
-#endif
 // }}}
 
 // give up
 #else
 // Diagnostics {{{
-#ifdef C_UNREACHABLE_DIAG
 #if C_UNREACHABLE_DIAG == 1
 #error c_unreachable method: none
 #elif C_UNREACHABLE_DIAG == 2
 #warning c_unreachable method: none
 #elif C_UNREACHABLE_DIAG == 3
 #pragma message("c_unreachable method: none")
-#endif
 #endif
 // }}}
 #define unreachable()
