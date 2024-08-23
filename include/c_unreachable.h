021e01126994bbd6ea582ccb888dbefa726137c3
yehuthi <yehuthith@gmail.com>
2024-08-23 12:00:22 +0300

    improve portability
    comply with ANSI (comments)

--- include/c_unreachable.h
+++ include/c_unreachable.h
@@ -1,15 +1,15 @@
 #ifndef __C_UNREACHABLE_H
 #define __C_UNREACHABLE_H
 
-// C23 setup
+/* C23 setup */
 #if __STDC_VERSION__ > 201710L
 #include <stddef.h>
 #endif
 
-// C23: check if actually implemented, as C23 is still pretty new
+/* C23: check if actually implemented, as C23 is still pretty new */
 #if __STDC_VERSION__ > 201710L && defined(unreachable)
-// then unreachable is provided, do nothing.
-// Diagnostics {{{
+/* then unreachable is provided, do nothing. */
+/* Diagnostics {{{ */
 #if C_UNREACHABLE_DIAG == 1
 #error c_unreachable method: C23
 #elif C_UNREACHABLE_DIAG == 2
@@ -17,12 +17,12 @@
 #elif C_UNREACHABLE_DIAG == 3
 #pragma message("c_unreachable method: C23")
 #endif
-// }}}
+/* }}} */
 
-// Compiler extensions
+/* Compiler extensions */
 #elif defined(__GNUC__)
 #define unreachable() (__builtin_unreachable())
-// Diagnostics {{{
+/* Diagnostics {{{ */
 #if C_UNREACHABLE_DIAG == 1
 #error c_unreachable method: extension GNU
 #elif C_UNREACHABLE_DIAG == 2
@@ -30,10 +30,10 @@
 #elif C_UNREACHABLE_DIAG == 3
 #pragma message("c_unreachable method: extension GNU")
 #endif
-// }}}
+/* }}} */
 #elif defined(_MSC_VER)
 #define unreachable() (__assume(0))
-// Diagnostics {{{
+/* Diagnostics {{{ */
 #if C_UNREACHABLE_DIAG == 1
 #error c_unreachable method: extension MSVC
 #elif C_UNREACHABLE_DIAG == 2
@@ -41,13 +41,13 @@
 #elif C_UNREACHABLE_DIAG == 3
 #pragma message("c_unreachable method: extension MSVC")
 #endif
-// }}}
+/* }}} */
 
-// C11 noreturn
+/* C11 noreturn */
 #elif __STDC_VERSION__ >= 201112L
 _Noreturn static inline void __c_unreachable_impl() {}
 #define unreachable() (__c_unreachable_impl())
-// Diagnostics {{{
+/* Diagnostics {{{ */
 #if C_UNREACHABLE_DIAG == 1
 #error c_unreachable method: noreturn
 #elif C_UNREACHABLE_DIAG == 2
@@ -55,11 +55,11 @@ _Noreturn static inline void __c_unreachable_impl() {}
 #elif C_UNREACHABLE_DIAG == 3
 #pragma message("c_unreachable method: noreturn")
 #endif
-// }}}
+/* }}} */
 
-// give up
+/* give up */
 #else
-// Diagnostics {{{
+/* Diagnostics {{{ */
 #if C_UNREACHABLE_DIAG == 1 || C_UNREACHABLE_DIAG == -1
 #error c_unreachable method: none
 #elif C_UNREACHABLE_DIAG == 2 || C_UNREACHABLE_DIAG == -2
@@ -67,8 +67,8 @@ _Noreturn static inline void __c_unreachable_impl() {}
 #elif C_UNREACHABLE_DIAG == 3 || C_UNREACHABLE_DIAG == -2
 #pragma message("c_unreachable method: none")
 #endif
-// }}}
+/* }}} */
 #define unreachable()
 #endif
 
-#endif // include guard
+#endif /* include guard */
